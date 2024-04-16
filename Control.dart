9d35f2a2b4f79a425c54e93e7304a300146940dc
FloAwandu <awanduflo@gmail.com>
2024-04-16 16:11:05 +0300

    first commit

---
+++ Control.dart
@@ -0,0 +1,24 @@
+void main() {
+  // Input a number of your choice
+  
+  int number = 12;
+
+  //print the message accordingly
+
+  if (number > 10) {
+
+    print('Your number is greater than 10');
+  } 
+  else if (number < 10) {
+    print('Your number is less than 10');
+  } 
+  else {
+    print('Your number is equal to 10');
+  }
+}
+
+
+
+
+
+
